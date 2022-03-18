ce60d2933c73d14eda1a15e64235343ee2e4222f
Taopol BERQUER <tao.berquer@gmail.com>
2022-03-18 15:17:48 +0100

    zz

--- back/server.go
+++ back/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/data", func(c *fiber.Ctx) error {
 		return c.JSON(getData("data.csv"))
