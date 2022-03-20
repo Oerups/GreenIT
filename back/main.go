124b804afe31e896246686f177a240df6103a1c1
LudovicCollignon <lud.collignon@gmail.com>
2022-03-20 21:11:58 +0100

    lkjkl

---
+++ back/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"os"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	app.Get("/data", func(c *fiber.Ctx) error {
+		return c.JSON(getData("data.csv"))
+	})
+
+	app.Get("/search", func(c *fiber.Ctx) error {
+		regions := c.Query("regions")
+		departments := c.Query("departments")
+		municipalities := c.Query("municipalities")
+		return c.JSON(search(regions, departments, municipalities))
+	})
+    port := os.Getenv("PORT")
+	app.Listen(":" + port)
+}
